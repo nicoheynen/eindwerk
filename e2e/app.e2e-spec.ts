8c26b93a29431b577de2003b278d34f45dddfe82
Angular CLI <angular-cli@angular.io>
2017-04-09 13:20:06 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { EindwerkPage } from './app.po';
+
+describe('eindwerk App', () => {
+  let page: EindwerkPage;
+
+  beforeEach(() => {
+    page = new EindwerkPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
